7c55db7c705f8f3f00341c730c1f6277bd2b5914
Example113355 <109909771+Example113355@users.noreply.github.com>
2024-04-16 23:22:47 +0700

    Update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,32 +5,6 @@ import { ApiBadRequestResponse, ApiConflictResponse, ApiCreatedResponse } from '
 import {UserDTO} from './users.dto';
 import e from 'express';
 
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Library API",
-      version: "1.0.0",
-      description: "A simple Express Library API",
-      termsOfService: "http://example.com/terms/",
-      contact: {
-        name: "API Support",
-        url: "http://www.exmaple.com/support",
-        email: "support@example.com",
-      },
-    },
-    servers: [
-      {
-        url: "https://nodejs-swagger-api.vercel.app/",
-        description: "My API Documentation",
-      },
-    ],
-  },
-  // This is to call all the file
-  apis: ["src/**/*.js"],
-};
-
 @Controller('users')
 @ApiTags('users')
 
